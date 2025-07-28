83ac9a296dd774f30713e3dac8ebeb27cd7c0152
Tonu <tonujk@gmail.com>
2025-07-28 14:17:38 +0100

    Commit after Exception and Logging

--- networksecurity/my_logging/my_logger.py
+++ networksecurity/my_logging/my_logger.py
@@ -4,14 +4,12 @@ from datetime import datetime
 
 # create Log file name format
 LOG_FILE_NAME = f"{datetime.now().strftime('%Y_%m_%d__%HHr_%Mm_%Ss')}.log"
-print(LOG_FILE_NAME)
 
 # Create the log file save location 
 log_dir_path = os.path.join(os.getcwd(), "Logs")
 os.makedirs(log_dir_path, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(log_dir_path, LOG_FILE_NAME)  # Complete file path
-print(LOG_FILE_PATH)
 
 logging.basicConfig(
     filename= LOG_FILE_PATH,
