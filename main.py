6f9d38028a01338bc121bf16b4e87513906d230b
Tonu <tonujk@gmail.com>
2025-07-29 19:47:35 +0100

    Commit after Data Validation

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from networksecurity.my_components.data_ingestion import DataIngestion
+from networksecurity.my_components.data_validation import DataValidation
  
 from networksecurity.my_exception.my_exception import NetworkSecurityException
 from networksecurity.my_logging.my_logger import logging
-from networksecurity.my_entity.config_entity import DataIngestionConfig
+from networksecurity.my_entity.config_entity import DataIngestionConfig, DataValidationConfig
 from networksecurity.my_entity.config_entity import TrainingPipelineConfig
 
 import sys
@@ -17,6 +18,12 @@ if __name__=='__main__':
         logging.info("Data Initiation Completed")
         print(dataingestionartifact) 
         
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifact,data_validation_config)
+        logging.info("Initiate the data Validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("data Validation Completed")
+        print(data_validation_artifact)
         
     except Exception as e:
            raise NetworkSecurityException(e,sys)
